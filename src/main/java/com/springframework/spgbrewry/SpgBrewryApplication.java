6db1640f728df5b7e4f340326162208a0a64defe
Dheeraj Joshi <dheerajjoshi157@gmail.com>
2022-01-20 20:54:15 +0530

    my first commit

---
+++ src/main/java/com/springframework/spgbrewry/SpgBrewryApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spgbrewry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgBrewryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgBrewryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/spgbrewry/SpgBrewryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spgbrewry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgBrewryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
